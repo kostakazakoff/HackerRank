e051ce29882302aca1a6486531d8c761699b86ee
Kosta Kazakov <kostakazakoff@gmail.com>
2023-01-01 22:26:07 +0200

    coding

---
+++ time_delta.py
@@ -0,0 +1,23 @@
+import os
+from datetime import datetime
+
+
+def time_delta(t1, t2):
+    date1_obj = datetime.strptime(t1, "%a %d %b %Y %H:%M:%S %z")
+    date2_obj = datetime.strptime(t2, "%a %d %b %Y %H:%M:%S %z")
+    diff = date2_obj - date1_obj
+    seconds_diff = diff.total_seconds()
+    return str(int(abs(seconds_diff)))
+
+
+if __name__ == '__main__':
+
+    with open(os.environ['OUTPUT_PATH'], 'w') as f:
+
+        t = int(input())
+
+        for _ in range(t):
+            t1 = input()
+            t2 = input()
+            delta = time_delta(t1, t2)
+            f.write(delta + '\n')

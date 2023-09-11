e630c07e9556a119480572363d1a404b271b8dd6
Kosta Kazakov <kostakazakoff@gmail.com>
2023-09-11 10:59:09 +0300

    problem solved

---
+++ migratory_birds.py
@@ -0,0 +1,33 @@
+import math
+import os
+import random
+import re
+import sys
+
+
+def migratoryBirds(arr):
+    # arr = sorted(arr, reverse=True)
+    # birds_dict = {arr.count(k): k for k in arr}
+    # print(birds_dict)
+    # birds_dict = dict(sorted(birds_dict.items(), key=lambda x: -x[0]))
+    # return list(birds_dict.values())[0]
+
+    arr = sorted(arr, reverse=True)
+    nums = set(arr)
+    birds_dict = {k:arr.count(k) for k in nums}
+    return max(birds_dict, key=birds_dict.get)
+
+
+if __name__ == '__main__':
+    # fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    arr_count = int(input().strip())
+
+    arr = list(map(int, input().rstrip().split()))
+
+    result = migratoryBirds(arr)
+    print(result)
+
+    # fptr.write(str(result) + '\n')
+
+    # fptr.close()

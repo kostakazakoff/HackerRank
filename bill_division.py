27dbe3027caeef5ca2637973763c97a76cea2167
Kosta Kazakov <kostakazakoff@gmail.com>
2024-04-16 23:05:39 +0300

    "Bill division" problem solved

---
+++ bill_division.py
@@ -0,0 +1,38 @@
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'bonAppetit' function below.
+#
+# The function accepts following parameters:
+#  1. INTEGER_ARRAY bill
+#  2. INTEGER k
+#  3. INTEGER b
+#
+
+def bonAppetit(bill, k, b):
+    total = sum(bill)
+    anna_bill = int((total - bill.pop(k)) / 2)
+    diff = b - anna_bill
+    if diff == 0:
+        diff = 'Bon Appetit'
+
+    print(diff)
+
+
+
+if __name__ == '__main__':
+    first_multiple_input = input().rstrip().split()
+
+    n = int(first_multiple_input[0])
+
+    k = int(first_multiple_input[1])
+
+    bill = list(map(int, input().rstrip().split()))
+
+    b = int(input().strip())
+
+    bonAppetit(bill, k, b)
